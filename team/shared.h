84f0879bda5db8ae793b2e6a8830a80fe1c225ba
Morgan Gallant <morgan@morgangallant.com>
2019-07-24 14:42:19 -0400

    added random number sequence generator

---
+++ team/shared.h
@@ -0,0 +1,11 @@
+#ifndef TEAM_SHARED_H_
+#define TEAM_SHARED_H_
+
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+
+// Generate a random sequence of length n.
+std::vector<int> generate_random_sequence(const int n);
+
+#endif  // TEAM_SHARED_H_
